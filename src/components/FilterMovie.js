a4607207bda10c8fc0147038c599073ecd488594
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-04 14:35:28 +0200

    template FilterMovie

---
+++ src/components/FilterMovie.js
@@ -0,0 +1,10 @@
+const FilterMovie = () => {
+  return (
+    <section>
+      <label htmlFor="movie">Movie </label>
+      <input type="text" id="movie" />
+    </section>
+  );
+};
+
+export default FilterMovie;
