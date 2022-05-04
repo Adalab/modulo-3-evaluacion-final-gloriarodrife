64f5e61c7b10060e4ef3261ef5ccf9d5022e0062
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-05 01:04:10 +0200

    order data

--- src/components/App.js
+++ src/components/App.js
@@ -16,17 +16,13 @@ function App() {
   useEffect(() => {
     callToApi().then((response) => {
       setLoaded(true);
-      const orderedMovies = sorteAlphabetically(response);
+      const orderedMovies = response.sort((a, b) =>
+        a.movie > b.movie ? 1 : a.movie < b.movie ? -1 : 0
+      );
       setData(orderedMovies);
     });
   }, []);
 
-  // Funcion para ordenar la lista
-  const sorteAlphabetically = (list) => {
-    return list.sort((a, b) =>
-      a.movie > b.movie ? 1 : a.movie < b.movie ? -1 : 0
-    );
-  };
   // Recogemos y guardamos el valor del input para filtrar las peliculas
   const handleFilterMovie = (value) => {
     setFilterMovie(value);
