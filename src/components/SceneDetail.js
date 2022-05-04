741ea037fdb969f5f11e30f4831381500193beaf
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-04 21:50:35 +0200

    message not movie scene

--- src/components/SceneDetail.js
+++ src/components/SceneDetail.js
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
 
 const MovieDetail = (props) => {
+  if (!props.movie) {
+    return <p>The scene you are looking for does not exist</p>;
+  }
+
   const { movie, image, phrase, director, audio } = props.movie;
   return (
     <>
