a9fe079d8d65b6bacdb6a5faea179baf3997b4d7
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-04 14:35:39 +0200

    template Filters

---
+++ src/components/Filters.js
@@ -0,0 +1,13 @@
+import FilterMovie from './FilterMovie';
+import FilterYear from './FilterYear';
+
+const Filters = () => {
+  return (
+    <>
+      <FilterMovie />
+      <FilterYear />
+    </>
+  );
+};
+
+export default Filters;
