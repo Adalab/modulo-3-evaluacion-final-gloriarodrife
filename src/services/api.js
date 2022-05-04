d05dea7fe569d63723f1cc817d6136837781f3fd
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-04 14:22:28 +0200

    fetch API

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,20 @@
 // Fichero src/services/api.js
-const callToApi = async (search) => {
-  const URL_API = `https://api.tvmaze.com`;
+const callToApi = async () => {
+  const URL_API = `https://owen-wilson-wow-api.herokuapp.com`;
 
   // Llamamos a la API
-  const response = await fetch(`${URL_API}/search/shows?q=${search}`);
+  const response = await fetch(`${URL_API}/wows/random?results=50`);
   const data = await response.json();
 
   // Cuando responde la API podemos limpiar los datos aquí
   const result = data.map((item) => {
     return {
-      name: item.show.name,
-      id: item.show.id,
+      movie: item.movie,
+      year: item.year,
+      audio: item.audio,
+      image: item.poster,
+      phrase: item.full_line,
+      director: item.director,
     };
   });
 
